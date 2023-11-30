4a689580c97f19df2334ebbaf388150502a09023
matheusjuliani <matheus.julianig@gmail.com>
2023-11-30 13:15:33 -0300

    grades

--- grades/src/application/Main.java
+++ grades/src/application/Main.java
@@ -1,7 +1,28 @@
 package application;
+import entities.Student;
 
+import java.util.Scanner;
+import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc =new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Student s1 = new Student();
+        System.out.print("Name: ");
+        s1.setName(sc.nextLine());
+        System.out.println("Digite as notas: ");
+        s1.setGrade1(sc.nextDouble());
+        s1.setGrade2(sc.nextDouble());
+        s1.setGrade3(sc.nextDouble());
+
+        s1.finalGrade();
+        s1.missingPoints();
+        System.out.printf("Final grade: %.2f%n",s1.finalGrade());
+        System.out.println();
+        if (s1.missingPoints() > 0){
+            System.out.printf("Missing points: %.2f%n",s1.missingPoints());
+        }else {
+            System.out.println("Pass");
+        }
     }
 }
--- grades/src/entities/Student.java
+++ grades/src/entities/Student.java
@@ -1,4 +1,54 @@
 package entities;
 
 public class Student {
+    private String name;
+    private double grade1;
+    private double grade2;
+    private double grade3;
+
+
+    //Getters & Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrade1() {
+        return grade1;
+    }
+
+    public void setGrade1(double grade1) {
+        this.grade1 = grade1;
+    }
+
+    public double getGrade2() {
+        return grade2;
+    }
+
+    public void setGrade2(double grade2) {
+        this.grade2 = grade2;
+    }
+
+    public double getGrade3() {
+        return grade3;
+    }
+
+    public void setGrade3(double grade3) {
+        this.grade3 = grade3;
+    }
+
+    public double finalGrade(){
+        return this.grade1 + this.grade2 + this.grade3;
+    }
+    public double missingPoints(){
+        if (finalGrade() < 60){
+            return 60 - finalGrade();
+        }else {
+            return 0.0;
+        }
+    }
 }
